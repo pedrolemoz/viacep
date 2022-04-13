eb397521df6de8c43893595713d9cadb5cce7889
Pedro Lemos <lemozxpedro@gmail.com>
2022-04-12 23:17:34 -0300

    feat: finished datasource tests

--- test/app/external/datasources/location_datasource_implementation_test.dart
+++ test/app/external/datasources/location_datasource_implementation_test.dart
@@ -4,10 +4,12 @@ import 'package:viacep/app/domain/entities/location.dart';
 import 'package:viacep/app/domain/parameters/get_location_by_cep_parameters.dart';
 import 'package:viacep/app/external/datasources/location_datasource_implementation.dart';
 import 'package:viacep/app/infrastructure/datasources/location_datasource.dart';
+import 'package:viacep/app/infrastructure/errors/cep_exceptions.dart';
 import 'package:viacep/app/infrastructure/mappers/location_mapper.dart';
 import 'package:viacep/core/endpoints/viacep_endpoints.dart';
 import 'package:viacep/core/packages/http_client/abstraction/http_client.dart';
 import 'package:viacep/core/packages/http_client/entities/http_response.dart';
+import 'package:viacep/core/packages/http_client/exceptions/server_exception.dart';
 
 import '../../../test_utils/mock_parser.dart';
 import '../../../test_utils/mocks_path.dart';
@@ -63,5 +65,81 @@ void main() {
     );
     verify(() => httpClientSpy.get(tEndpoint));
     verifyNoMoreInteractions(httpClientSpy);
+  });
+
+  test(
+      'Should throw UnableToGetLocationUsingCEPException if status code is 200 but the data is invalid',
+      () async {
+    // Arrange
+    when(() => httpClientSpy.get(tEndpoint)).thenAnswer(
+      (_) async => HttpResponse(
+        url: tEndpoint,
+        data: 'Invalid response',
+        statusCode: 200,
+      ),
+    );
+
+    // Act
+    final call = dataSource.getLocationByCEP;
+
+    // Assert
+    expect(
+      () => call(tParameters),
+      throwsA(
+        isA<UnableToGetLocationUsingCEPException>(),
+      ),
+    );
+  });
+
+  test('Should throw InvalidCEPFormatException if status code is 400',
+      () async {
+    // Arrange
+    when(() => httpClientSpy.get(tEndpoint)).thenAnswer(
+      (_) async => HttpResponse(
+        url: tEndpoint,
+        data: 'Bad request',
+        statusCode: 400,
+      ),
+    );
+
+    // Act
+    final call = dataSource.getLocationByCEP;
+
+    // Assert
+    expect(
+      () => call(tParameters),
+      throwsA(
+        isA<InvalidCEPFormatException>(),
+      ),
+    );
+  });
+
+  test('Should throw ServerException if status code is anything else',
+      () async {
+    // Arrange
+    const tData = 'Internal server error';
+    const tStatusCode = 500;
+    when(() => httpClientSpy.get(tEndpoint)).thenAnswer(
+      (_) async => HttpResponse(
+        url: tEndpoint,
+        data: tData,
+        statusCode: tStatusCode,
+      ),
+    );
+
+    // Act
+    final call = dataSource.getLocationByCEP;
+
+    // Assert
+    expect(
+      () => call(tParameters),
+      throwsA(
+        isA<ServerException>().having(
+          (exception) => exception.data,
+          'Has the body of request',
+          '$tStatusCode - $tData',
+        ),
+      ),
+    );
   });
 }
